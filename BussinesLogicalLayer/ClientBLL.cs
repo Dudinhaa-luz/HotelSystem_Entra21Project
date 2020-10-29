7d14addf8968c5788b2211f4d27935a671dd43c5
Heloisa Vendel Theiss <helo_PB@outlook.com>
2020-10-29 16:46:41 -0300

    Atualização BLL

---
+++ BussinesLogicalLayer/ClientBLL.cs
@@ -0,0 +1,188 @@
+﻿using BussinesLogicalLayer.Extensions;
+using Common;
+using DataAccessObject;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinesLogicalLayer
+{
+    public class ClientBLL : BaseValidator<Client>
+    {
+        private ClientDAO clientDAO = new ClientDAO();
+
+        public Response Insert(Client item)
+        {
+            Response response = new Response();
+            if (response.Success)
+            {
+                return clientDAO.Insert(item);
+            }
+            return response;
+        }
+
+        public Response Update(Client item)
+        {
+            Response response = new Response();
+            if (response.Success)
+            {
+                return clientDAO.Update(item);
+            }
+            return response;
+        }
+
+        public Response Delete(Client item)
+        {
+            return clientDAO.Delete(item);
+        }
+
+        public QueryResponse<Client> GetAllClientsByActive()
+        {
+            QueryResponse<Client> responseClients = clientDAO.GetAllClientsByActive();
+            List<Client> temp = responseClients.Data;
+            foreach (Client item in temp)
+            {
+                item.CPF = item.CPF.Insert(3, ".").Insert(7, ".").Insert(12, "-");
+                item.RG = item.RG.Insert(1, ".").Insert(4, ".");
+                item.PhoneNumber1 = item.PhoneNumber1.Insert(0, "+").Insert(3, "(").Insert(6, ")").Insert(12, "-");
+                if (item.PhoneNumber2 != null)
+                {
+                    item.PhoneNumber2 = item.PhoneNumber2.Insert(0, "+").Insert(3, "(").Insert(6, ")").Insert(12, "-");
+                }
+            }
+            return responseClients;
+        }
+
+        public QueryResponse<Client> GetAllClientsByInactive()
+        {
+            QueryResponse<Client> responseClients = clientDAO.GetAllClientsByActive();
+            List<Client> temp = responseClients.Data;
+            foreach (Client item in temp)
+            {
+                item.CPF = item.CPF.Insert(3, ".").Insert(7, ".").Insert(12, "-");
+                item.RG = item.RG.Insert(1, ".").Insert(4, ".");
+                item.PhoneNumber1 = item.PhoneNumber1.Insert(0, "+").Insert(3, "(").Insert(6, ")").Insert(12, "-");
+                if (item.PhoneNumber2 != null)
+                {
+                    item.PhoneNumber2 = item.PhoneNumber2.Insert(0, "+").Insert(3, "(").Insert(6, ")").Insert(12, "-");
+                }
+            }
+            return responseClients;
+        }
+
+        public QueryResponse<Client> GetAllClientsByName()
+        {
+            QueryResponse<Client> responseClients = clientDAO.GetAllClientsByActive();
+            List<Client> temp = responseClients.Data;
+            foreach (Client item in temp)
+            {
+                item.CPF = item.CPF.Insert(3, ".").Insert(7, ".").Insert(12, "-");
+                item.RG = item.RG.Insert(1, ".").Insert(4, ".");
+                item.PhoneNumber1 = item.PhoneNumber1.Insert(0, "+").Insert(3, "(").Insert(6, ")").Insert(12, "-");
+                if (item.PhoneNumber2 != null)
+                {
+                    item.PhoneNumber2 = item.PhoneNumber2.Insert(0, "+").Insert(3, "(").Insert(6, ")").Insert(12, "-");
+                }
+            }
+            return responseClients;
+        }
+        public QueryResponse<Client> GetAllClientsByCPF()
+        {
+            QueryResponse<Client> responseClients = clientDAO.GetAllClientsByActive();
+            List<Client> temp = responseClients.Data;
+            foreach (Client item in temp)
+            {
+                item.CPF = item.CPF.Insert(3, ".").Insert(7, ".").Insert(12, "-");
+                item.RG = item.RG.Insert(1, ".").Insert(4, ".");
+                item.PhoneNumber1 = item.PhoneNumber1.Insert(0, "+").Insert(3, "(").Insert(6, ")").Insert(12, "-");
+                if (item.PhoneNumber2 != null)
+                {
+                    item.PhoneNumber2 = item.PhoneNumber2.Insert(0, "+").Insert(3, "(").Insert(6, ")").Insert(12, "-");
+                }
+            }
+            return responseClients;
+        }
+
+        public QueryResponse<Client> GetClientsByID()
+        {
+            QueryResponse<Client> responseClients = clientDAO.GetAllClientsByActive();
+            List<Client> temp = responseClients.Data;
+            foreach (Client item in temp)
+            {
+                item.CPF = item.CPF.Insert(3, ".").Insert(7, ".").Insert(12, "-");
+                item.RG = item.RG.Insert(1, ".").Insert(4, ".");
+                item.PhoneNumber1 = item.PhoneNumber1.Insert(0, "+").Insert(3, "(").Insert(6, ")").Insert(12, "-");
+                if (item.PhoneNumber2 != null)
+                {
+                    item.PhoneNumber2 = item.PhoneNumber2.Insert(0, "+").Insert(3, "(").Insert(6, ")").Insert(12, "-");
+                }
+            }
+            return responseClients;
+        }
+
+        public override Response Validate(Client item)
+        {
+            if (string.IsNullOrWhiteSpace(item.Name))
+            {
+                AddError("O nome deve ser informado.");
+            }
+            else if (item.Name.Length < 3 || item.Name.Length < 80)
+            {
+                AddError("O nome deve conter entre 3 e 80 caracteres.");
+            }
+            for (int i = 0; i < item.Name.Length; i++)
+            {
+                if (!char.IsLetter(item.Name[i]))
+                {
+                    AddError("O nome deve contêr apenas letras.");
+                }
+            }
+            if (string.IsNullOrWhiteSpace(item.PhoneNumber1))
+            {
+                AddError("O telefone deve ser informado!");
+            }
+            else if (item.PhoneNumber1.Length < 12)
+            {
+                AddError("O telefone deve contêr 12 caracteres");
+            }
+            if (item.PhoneNumber2 != null)
+            {
+                if (item.PhoneNumber2.Length < 12)
+                {
+                    AddError("O telefone deve contêr 12 caracteres");
+                }
+            }
+            if (string.IsNullOrWhiteSpace(item.RG))
+            {
+                AddError("O RG deve ser informado!");
+            }
+            else if (item.RG.Length != 7)
+            {
+                AddError("O RG deve contêr 7 caracteres.");
+            }
+            for (int i = 0; i < item.RG.Length; i++)
+            {
+                if (char.IsLetter(item.RG[i]))
+                {
+                    AddError("RG deve contêr apenas números.");
+                }
+            }
+            
+            AddError(item.PhoneNumber1.IsValidPhoneNumber());
+
+            AddError(item.PhoneNumber2.IsValidPhoneNumber());
+
+            AddError(item.CPF.IsValidCPF());
+
+            AddError(item.Email.IsValidEmail());
+
+            Response responseCPF = clientDAO.IsCPFUnique(item.CPF);
+            if (!responseCPF.Success)
+            {
+                AddError(responseCPF.Message);
+            }
+            return base.Validate(item);
+        }
+    }
+}
---
+++ BussinesLogicalLayer/Extensions/StringExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BussinesLogicalLayer.Extensions
+{
+    static class StringExtensions
+    {
+		public static string IsValidCPF(this string cpf)
+		{
+			int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+			int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+			string tempCpf;
+			string digito;
+			int soma;
+			int resto;
+			cpf = cpf.Trim();
+			cpf = cpf.Replace(".", "").Replace("-", "");
+			if (cpf.Length != 11)
+				return "CPF deve conter 11 caracteres.";
+			tempCpf = cpf.Substring(0, 9);
+			soma = 0;
+
+			for (int i = 0; i < 9; i++)
+				soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+			resto = soma % 11;
+			if (resto < 2)
+				resto = 0;
+			else
+				resto = 11 - resto;
+			digito = resto.ToString();
+			tempCpf = tempCpf + digito;
+			soma = 0;
+			for (int i = 0; i < 10; i++)
+				soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+			resto = soma % 11;
+			if (resto < 2)
+				resto = 0;
+			else
+				resto = 11 - resto;
+			digito = digito + resto.ToString();
+			bool val = cpf.EndsWith(digito);
+			if (val)
+			{
+				return "";
+			}
+			return "CPF inválido.";
+		}
+        public static string IsValidPhoneNumber (this string phonenumber)
+		{
+			Regex regex = new Regex(@"^(\([0 - 9]{ 2 }\))\s([9]{ 1})?([0 - 9]{ 4})-([0 - 9]{ 4})$");
+            if (regex.IsMatch(phonenumber))
+            {
+				return "";
+            }
+			return "Telefone inválido";
+		}
+        public static string IsValidEmail (this string email)
+        {
+			
+				Regex regex = new Regex(@"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}");
+            if (regex.IsMatch(email))
+            {
+				return "";
+            }
+				return "Email inválido";
+		}
+    }
+}
