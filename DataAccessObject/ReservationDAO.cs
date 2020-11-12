7f123b8e7b49f31b1247e9fb6789fff53a690ee7
Lucasilec <lucasileck203@gmail.com>
2020-11-12 16:36:34 -0300

    Revisão Reservation Entities (Fazer o DAO)

--- DataAccessObject/ReservationDAO.cs
+++ DataAccessObject/ReservationDAO.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessObject
 {
+
     public class ReservationDAO
     {
         public Response Insert(Reservation reservation)
--- Entities/Reservation.cs
+++ Entities/Reservation.cs
@@ -10,6 +10,7 @@ namespace Entities
     {
         public int ID { get; set; }
         public DateTime ReservationDate { get; set; }
+        public DateTime ExitDatePrevision { get; set; }
         public int RoomID { get; set; }
         public int ClientID { get; set; }
     }
