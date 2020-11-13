76b961d4342dff73b1b61860cd1b149cd7031a36
Lucasilec <lucasileck203@gmail.com>
2020-11-13 14:18:21 -0300

    Revisão BLL 1

--- BussinesLogicalLayer/RoomBLL.cs
+++ BussinesLogicalLayer/RoomBLL.cs
@@ -59,12 +59,12 @@ namespace BussinesLogicalLayer {
 
             if (string.IsNullOrWhiteSpace(item.NumberRoom)) {
                 AddError("O número do quarto deve ser informado.");
-            } else if (item.NumberRoom.Length < 1 || item.NumberRoom.Length < 5) {
+            } else if (item.NumberRoom.Length < 1 || item.NumberRoom.Length > 5) {
                 AddError("O número do quarto deve conter entre 1 e 5 caracteres");
             }
             if (string.IsNullOrWhiteSpace(item.Description)) {
                 AddError("A descrição do quarto deve ser informada.");
-            } else if (item.NumberRoom.Length < 1 || item.NumberRoom.Length < 5) {
+            } else if (item.NumberRoom.Length < 3 || item.NumberRoom.Length > 100) {
                 AddError("A descrição deve conter entre 3 e 100 caracteres");
             }
             if (item.IDRoomType == 0) {
--- Entities/QueryModel/ReservationQueryModel.cs
+++ Entities/QueryModel/ReservationQueryModel.cs
@@ -8,6 +8,7 @@ namespace Entities.QueryModel {
     public class ReservationQueryModel {
         public int ReservationID { get; set; }
         public DateTime ReservationDate { get; set; }
+        public DateTime ReservationExitDatePrevision{ get; set; }
         public string RoomNumber { get; set; }
         public string ClientName { get; set; }
         public string ClientCPF { get; set; }
