b76101c02ed85fe099ecb1d4ec0365b7e3e03b24
Dudinhaa-luz <eduardaluz006@gmail.com>
2020-11-18 09:02:02 -0300

    ...

--- PresentationLayer/FormsReservation/FormLinkClient.Designer.cs
+++ PresentationLayer/FormsReservation/FormLinkClient.Designer.cs
@@ -1,6 +1,5 @@
-﻿namespace PresentationLayer
-{
-    partial class FormLinkRoom {
+﻿namespace PresentationLayer {
+    partial class FormLinkClient {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -10,10 +9,8 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -25,8 +22,7 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
